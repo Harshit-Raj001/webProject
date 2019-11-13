e31d13aaec50d8d3fc9b6d4098d64c9013daf9b4
Harshit Rajput <45284131+Harshit-Raj001@users.noreply.github.com>
2019-11-13 12:49:24 +0530

    file_update
    updated files

--- login.php
+++ login.php
@@ -14,12 +14,14 @@
         die("connection failed: ".$sql_conn->connect_error);
     }
     if($utype == "student"){
+        /*
         $qq = "select Password, bdate from student where s_id = $eid";
         $qr = $sql_conn->query($qq);
         if($qr->num_rows == 0){
             //user not found error in the login page
             die("user not found");
         }
+        
         $tup = $qr->fetch_assoc()
         if($tup["Password"]===$pass && $tup["bdate"]==$dob){
             //open student's access page
@@ -27,9 +29,49 @@
         else{
             //pass or dob is incorrect
             die("Details might be incorrect");
+        }*/
+        $qq = "select fname from student where s_id=$eid and Password=$pass and bdate=$dob";
+        $qr = $sql_conn->query($qq);
+        if($qr->num_rows == 0){
+            //user details incorrect error on login page
+            die("Details might be incorrect");
+        }
+        else{
+            //open students access page
         }
     }
-    if($utype == "admin"){}
-    if($utype == "teacher"){}
-    if($utype == "non_staff"){}
+    if($utype == "admin"){
+        $qq = "select fname from admin where e_id=$eid and Password=$pass";
+        $qr = $sql_conn->query($qq);
+        if($qr->numm_row == 0){
+            //user details incorrect error on login page
+            die("Details might be incorrect");
+        }
+        else{
+            //open admin's access page
+        }
+    }
+    if($utype == "teacher"){
+        $qq = "select fname from teacher where e_id=$eid and Password=$pass";
+        $qr = $sql_conn->query($qq);
+        if($qr->num_rows == 0){
+            //user details incorrect error on login page
+            die("Details might be incorrect");
+        }
+        else{
+            //open teacher's acccess page
+        }
+    }
+    if($utype == "non_staff"){
+        $qq = "select fname from non_staff where e_id=$eid and Password=$pass";
+        $qr = $sql_conn->query($qq);
+        if($qr->num_rows == 0){
+            //user details incorrect error on login page
+            die("Details might be incorrect");
+        }
+        else{
+            //open non_staff's access page
+        }
+    }
+    $sql_conn->close();
 >
---
+++ tempphp.php
@@ -0,0 +1,7 @@
+<?php
+    $n = "";
+    $n = $_POST["f"]["chck"];
+    if($n < 10){
+        header("Location: C:\Users\tush\Documents\GitHub\webProject\login.htm");
+    }
+>
