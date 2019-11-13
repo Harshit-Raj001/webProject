c3e69366ccf0e5993585eb249ff9fadd01030f7b
Harshit Rajput <45284131+Harshit-Raj001@users.noreply.github.com>
2019-11-13 19:21:03 +0530

    testing
    testing a few things

--- tempphp.php
+++ tempphp.php
@@ -1,7 +1,3 @@
 <?php
-    $n = "";
-    $n = $_POST["f"]["chck"];
-    if($n < 10){
-        header("Location: C:\Users\tush\Documents\GitHub\webProject\login.htm");
-    }
->
+    echo "works";
+?>
