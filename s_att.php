86fde25da2b1450346fbc6edfa972c2feaefb102
Amanjakhetiya <amanjakhetiya@gmail.com>
2019-11-19 00:23:29 +0530

    attendance

---
+++ s_att.php
@@ -0,0 +1,19 @@
+<?php
+    include("logincopy.htm");
+    $s=$_POST["EID"];
+    $con=mysqli_connect("localhost","root","","web_dev_project");
+    if (!$con)
+    {
+        die("connection failed");
+    }
+    $result=mysqli_query($con,"SELECT  attDate,status FROM s_att where s_id=$s;");
+    echo "<table>";
+    while($row=mysqli_fetch_array($result))
+    {
+        echo "<tr><td>".$row["attDate"]."</td>"."<td>".$row["status"]."</td></tr>";
+    }
+    echo "</table>";
+    $con.close();
+    ?>
+ 
+ 
