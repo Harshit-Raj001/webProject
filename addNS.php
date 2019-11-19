08f4012aee42b8c42ebf1563956092eb751fc54e
Harshit Rajput <45284131+Harshit-Raj001@users.noreply.github.com>
2019-11-19 16:17:47 +0530

    add user files

---
+++ addNS.php
@@ -0,0 +1,14 @@
+<?php
+        $eid = $_POST['eid'];
+        $pass = $_POST['pass'];
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $pno = $_POST['pno'];
+        $add = $_POST['addrss'];
+        $mail = $_POST['mail'];
+        $doj = $_POST['doj'];
+        $sql = new mysqli("localhost", "root", "", "web_dev_project");
+        $qq = "insert into non_staff values($eid,'$pass','$fname','$lname',$pno,'$add','$mail','$doj');";
+        $qr = $sql->query($qq);
+        $sql->close();
+    ?>
---
+++ addS.php
@@ -0,0 +1,14 @@
+<?php
+        $eid = $_POST['eid'];
+        $pass = $_POST['pass'];
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $pno = $_POST['pno'];
+        $add = $_POST['addrss'];
+        $mail = $_POST['mail'];
+        $dob = $_POST['dob'];
+        $sql = new mysqli("localhost", "root", "", "web_dev_project");
+        $qq = "insert into student values($eid,'$pass','$fname','$lname',$pno,'$add','$mail','$dob');";
+        $qr = $sql->query($qq);
+        $sql->close();
+    ?>
