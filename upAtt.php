63d56d74d36981fecef8dc498e9555d286736ea3
Harshit Rajput <45284131+Harshit-Raj001@users.noreply.github.com>
2019-11-20 23:09:45 +0530

    a

--- upAtt.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $sql = new mysqli("localhost", "root", "", "web_dev_project");
-    $dd; //currentdate
-    while($t--){
-        echo $dd."<input type="radio" value="p" id="date"><input type="radio" value="p" id="date">";
-    }
-    
-?>
